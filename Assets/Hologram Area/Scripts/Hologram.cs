3ca5a0d7f0ae9a7383e06cfd554ac4787180e1bc
Aaron <aaronm@kainos.com>
2017-07-25 11:44:05 +0100

    Small refactorization

--- Assets/Hologram Area/Scripts/Hologram.cs
+++ Assets/Hologram Area/Scripts/Hologram.cs
@@ -4,127 +4,122 @@ using System;
 
 public class Hologram : MonoBehaviour 
 {
-	public GameObject[] cameras;
+    public Camera[] cameras;
 
     public float hologramArea = 1f;
 
-	public enum Direction
-	{
-		topDown, bottomUp
-	};
+    public enum Direction
+    {
+        topDown, bottomUp
+    };
 
-	public Direction type = new Direction ();
+    public Direction type = new Direction();
 
-	void OnValidate()
-	{
-		Scale ();
-		CameraClipping ();
-		CameraRotation ();
-		CameraRect (GetMainGameViewSize());
-	}
+    void OnValidate()
+    {
+        //These methods only get called when an inspector value for the script has been modified.
+        Scale();
+        CameraClipping();
+        CameraRotation();
+        CameraRect(GetMainGameViewSize());
+    }
 
-	void Start()
-	{
-		CameraRect (new Vector2((float)Screen.width, (float)Screen.height));
-		StartCoroutine (FadeViewport ());
-		Screen.sleepTimeout = SleepTimeout.NeverSleep;
-	}
+    void Start()
+    {
+        CameraRect(new Vector2(Screen.width, Screen.height));
+        StartCoroutine(FadeViewport());
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
 
-	void Scale()
-	{
-		transform.localScale = new Vector3(hologramArea, hologramArea, hologramArea);
-	}
+    void Scale()
+    {
+        transform.localScale = new Vector3(hologramArea, hologramArea, hologramArea);
+    }
 
-	void CameraClipping()
-	{
-		float centerToEdge = hologramArea / 2f;
-		float EdgeToCamera = centerToEdge / (float)(Math.Tan (30 * (Math.PI / 180)));
+    void CameraClipping()
+    {
+        float centerToEdge = hologramArea / 2f;
+        float EdgeToCamera = centerToEdge / (float)(Math.Tan(30 * (Math.PI / 180)));
 
-		for (int i = 0; i < cameras.Length; i++)
-		{
-			cameras[i].GetComponent<Camera>().farClipPlane = EdgeToCamera + hologramArea;
-			cameras[i].GetComponent<Camera>().nearClipPlane = EdgeToCamera;
-		}
-	}
+        for (int i = 1; i < cameras.Length; i++)
+        {
+            cameras[i].farClipPlane = EdgeToCamera + hologramArea;
+            cameras[i].nearClipPlane = EdgeToCamera;
+        }
+    }
 
-	void CameraRotation()
-	{
-		if (type == Direction.topDown) 
-		{
-			cameras [1].transform.eulerAngles = new Vector3 (0, 0, 0);
-			cameras [2].transform.eulerAngles = new Vector3 (0, 180, 180);
-			cameras [3].transform.eulerAngles = new Vector3 (0, 90, 90);
-			cameras [4].transform.eulerAngles = new Vector3 (0, 270, 270);
-		}
-		else if (type == Direction.bottomUp) 
-		{
-			cameras [1].transform.eulerAngles = new Vector3 (0, 0, 180);
-			cameras [2].transform.eulerAngles = new Vector3 (0, 180, 0);
-			cameras [3].transform.eulerAngles = new Vector3 (0, 90, 90);
-			cameras [4].transform.eulerAngles = new Vector3 (0, 270, 270);
-		}
-	}
+    void CameraRotation()
+    {
+        if (type == Direction.topDown)
+        {
+            cameras[1].transform.eulerAngles = new Vector3(0, 0, 0);
+            cameras[2].transform.eulerAngles = new Vector3(0, 180, 180);
+        }
+        else if (type == Direction.bottomUp)
+        {
+            cameras[1].transform.eulerAngles = new Vector3(0, 0, 180);
+            cameras[2].transform.eulerAngles = new Vector3(0, 180, 0);
+        }
 
-	void CameraRect(Vector2 res)
-	{
-		float aspectRatio = res.y / res.x;
+        cameras[3].transform.eulerAngles = new Vector3(0, 90, 90);
+        cameras[4].transform.eulerAngles = new Vector3(0, 270, 270);
+    }
 
-		float cameraSizeX = 1 / 3f;
-		float cameraCenterX = 1 / 2f * cameraSizeX;
+    void CameraRect(Vector2 res)
+    {
+        float aspectRatio = res.y / res.x;
 
-		float cameraSizeY = 1 / 3f;
-		float cameraCenterY = 1 / 2f * cameraSizeY;
+        float cameraSizeX = 1 / 3f;
+        float cameraCenterX = 1 / 2f * cameraSizeX;
 
-		if (aspectRatio < 1) 
-		{
-			cameraSizeX = cameraSizeX * aspectRatio;
-			cameraCenterX = cameraCenterX * aspectRatio;
-		} 
-		else if (aspectRatio > 1) 
-		{
-			cameraSizeY = cameraSizeY / aspectRatio;
-			cameraCenterY = cameraCenterY / aspectRatio;
-		}
+        float cameraSizeY = 1 / 3f;
+        float cameraCenterY = 1 / 2f * cameraSizeY;
 
-		if (type == Direction.topDown) 
-		{
-			cameras [0].GetComponent<Camera> ().farClipPlane = 0.02f;
-			cameras [0].GetComponent<Camera> ().nearClipPlane = 0.01f;
-			cameras [1].GetComponent<Camera> ().rect = new Rect (1 / 2f - cameraCenterX, 1 / 2f - (3 * cameraCenterY), cameraSizeX, cameraSizeY);
-			cameras [2].GetComponent<Camera> ().rect = new Rect (1 / 2f - cameraCenterX, 1 / 2f + cameraCenterY, cameraSizeX, cameraSizeY);
-			cameras [3].GetComponent<Camera> ().rect = new Rect (1 / 2f - (3 * cameraCenterX), 1 / 2f - cameraCenterY, cameraSizeX, cameraSizeY);
-			cameras [4].GetComponent<Camera> ().rect = new Rect (1 / 2f + cameraCenterX, 1 / 2f - cameraCenterY, cameraSizeX, cameraSizeY); 
-		} 
-		else if (type == Direction.bottomUp) 
-		{
-			cameras [0].GetComponent<Camera> ().farClipPlane = 0.02f;
-			cameras [0].GetComponent<Camera> ().nearClipPlane = 0.01f;
-			cameras [1].GetComponent<Camera> ().rect = new Rect (1 / 2f - cameraCenterX, 1 / 2f - (3 * cameraCenterY), cameraSizeX, cameraSizeY);
-			cameras [2].GetComponent<Camera> ().rect = new Rect (1 / 2f - cameraCenterX, 1 / 2f + cameraCenterY, cameraSizeX, cameraSizeY);
-			cameras [3].GetComponent<Camera> ().rect = new Rect (1 / 2f + cameraCenterX, 1 / 2f - cameraCenterY, cameraSizeX, cameraSizeY);
-			cameras [4].GetComponent<Camera> ().rect = new Rect (1 / 2f - (3 * cameraCenterX), 1 / 2f - cameraCenterY, cameraSizeX , cameraSizeY); 	 
-		}
-	}
+        if (aspectRatio < 1)
+        {
+            cameraSizeX = cameraSizeX * aspectRatio;
+            cameraCenterX = cameraCenterX * aspectRatio;
+        }
+        else if (aspectRatio > 1)
+        {
+            cameraSizeY = cameraSizeY / aspectRatio;
+            cameraCenterY = cameraCenterY / aspectRatio;
+        }
 
-	Vector2 GetMainGameViewSize()
-	{
-		System.Type T = System.Type.GetType("UnityEditor.GameView,UnityEditor");
-		System.Reflection.MethodInfo GetSizeOfMainGameView = T.GetMethod("GetSizeOfMainGameView", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
-		System.Object Res = GetSizeOfMainGameView.Invoke(null, null);
-		return (Vector2)Res;
-	}
+        cameras[1].rect = new Rect(1 / 2f - cameraCenterX, 1 / 2f - (3 * cameraCenterY), cameraSizeX, cameraSizeY);
+        cameras[2].rect = new Rect(1 / 2f - cameraCenterX, 1 / 2f + cameraCenterY, cameraSizeX, cameraSizeY);
 
-	IEnumerator FadeViewport()
-	{
-		float elapsedTime = 0f;
-		float time = 5f;
+        if (type == Direction.topDown)
+        {
+            cameras[3].rect = new Rect(1 / 2f - (3 * cameraCenterX), 1 / 2f - cameraCenterY, cameraSizeX, cameraSizeY);
+            cameras[4].rect = new Rect(1 / 2f + cameraCenterX, 1 / 2f - cameraCenterY, cameraSizeX, cameraSizeY);
+        }
+        else if (type == Direction.bottomUp)
+        {
+            cameras[3].rect = new Rect(1 / 2f + cameraCenterX, 1 / 2f - cameraCenterY, cameraSizeX, cameraSizeY);
+            cameras[4].rect = new Rect(1 / 2f - (3 * cameraCenterX), 1 / 2f - cameraCenterY, cameraSizeX, cameraSizeY);
+        }
+    }
 
-		while (elapsedTime < time) 
-		{ 
-			float color = Mathf.Lerp(1f, 0f, elapsedTime / time);
-			cameras[0].GetComponent<Camera>().backgroundColor = new Color(color, color, color, color);
-			elapsedTime += Time.deltaTime;
-			yield return new WaitForEndOfFrame();
-		}
-	}
+    Vector2 GetMainGameViewSize()
+    {
+        Type T = Type.GetType("UnityEditor.GameView,UnityEditor");
+        System.Reflection.MethodInfo GetSizeOfMainGameView = T.GetMethod("GetSizeOfMainGameView", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+        System.Object Res = GetSizeOfMainGameView.Invoke(null, null);
+        return (Vector2)Res;
+    }
+
+    IEnumerator FadeViewport()
+    {
+        float elapsedTime = 0f;
+        float time = 5f;
+
+        while (elapsedTime < time)
+        {
+            float color = Mathf.Lerp(1f, 0f, elapsedTime / time);
+            cameras[0].backgroundColor = new Color(color, color, color, color);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
 }
